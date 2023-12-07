6532baee91ad20db93493383cfef7d4a817cac78
Murilo Júlio da Silveira Negrão <murilonegrao@gmail.com>
2023-12-07 00:34:09 -0300

    alteração para editar e excluir eventos

--- dio_django/core/models.py
+++ dio_django/core/models.py
@@ -13,8 +13,14 @@ class Evento(models.Model):
     class Meta:
         db_table = 'evento'
 
+
     def __str__(self):
         return str(self.titulo)
     
+
     def get_data_evento(self):
         return self.data_evento.strftime('%d/%m/%Y %H:%M')
+    
+
+    def get_data_input_evento(self):
+        return self.data_evento.strftime('%Y-%m-%dT%H:%M')
