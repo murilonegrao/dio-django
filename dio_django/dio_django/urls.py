6e9e2dafbf248222b55292656b821974a340a9fa
Murilo Júlio da Silveira Negrão <murilonegrao@gmail.com>
2023-12-06 23:33:16 -0300

    template Evento

--- dio_django/dio_django/urls.py
+++ dio_django/dio_django/urls.py
@@ -23,5 +23,10 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("hello/<nome>/<int:idade>/", views.hello),
     path("agenda/", views.lista_eventos),
-    path("", RedirectView.as_view(url='/agenda/'))
+    path("agenda/evento/", views.evento),
+    path("agenda/evento/submit", views.submit_evento),
+    path("", RedirectView.as_view(url='/agenda/')),
+    path("login/", views.login_user),
+    path("login/submit", views.submit_login),
+    path('logout/', views.logout_user),
 ]
